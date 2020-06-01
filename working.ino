55fb830cd78f8dd5c6cd4f867d5e28b04e5719bb
KindSpidey <44064808+KindSpidey@users.noreply.github.com>
2020-06-01 18:12:04 +0500

    Update working.ino

--- working.ino
+++ working.ino
@@ -1,11 +1,11 @@
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
-#include "pitches.h"
+//#include "pitches.h"
 #include <Thread.h>
-
+ 
 Thread soundThread = Thread();
 Thread ledThread = Thread();
-
+ 
 int PIR = 6;
 int redLed = 5;
 int bluLed = 4;
@@ -22,19 +22,19 @@ bool stopalarm = 1;
 byte sa = 1;
 const char *ssid = "xxxxxxxxx"; // Имя роутера
 const char *pass = "xxxxxxxxx"; // Пароль роутера
-
+ 
 const char *mqtt_server = "xxxxxxxxx"; // Имя сервера MQTT
 const int mqtt_port = 1883; // Порт для подключения к серверу MQTT
 const char *mqtt_user = "xxxxxxxxx"; // Логи для подключения к серверу MQTT
 const char *mqtt_pass = "xxxxxxxxx"; // Пароль для подключения к серверу MQTT
-const char* mqtt_topics[] = {"devices/1/phone/", "devices/1/email/", 
-                             "devices/1/led1/", "devices/1/sound/", 
-                           "devices/1/pir1/", "devices/1/timetokill/phone/", 
+const char* mqtt_topics[] = {"devices/1/phone/", "devices/1/email/",
+                             "devices/1/led1/", "devices/1/sound/",
+                           "devices/1/pir1/", "devices/1/timetokill/phone/",
                       "devices/1/timetokill/email/", "devices/1/stopalarm/",
-			    "devices/1/timetokill/sms/", "devices/1/situation/"};
-
-WiFiClient wclient; 
-
+                "devices/1/timetokill/sms/", "devices/1/situation/"};
+ 
+WiFiClient wclient;
+ 
 void setup()
 {
   pinMode(soundPin, OUTPUT);
@@ -48,17 +48,15 @@ void setup()
   sound = 1;
   timer = millis();
   soundThread.onRun(playmelody);     // назначаем потоку задачу
-  soundThread.setInterval(20); 
+  soundThread.setInterval(20);
   ledThread.onRun(ledBlink);  // назначаем потоку задачу
   ledThread.setInterval(1000);
 }
-
+ 
 void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.println("]");
-	byte* buffer;
-	CString str((const wchar_t*)buffer);
    if(strcmp(topic, "devices/1/phone/")==0){
    phone = int(payload);}
    if(strcmp(topic, "devices/1/email/")==0){
@@ -69,18 +67,18 @@ void callback(char* topic, byte* payload, unsigned int length) {
    sound = int(payload);}
    if(strcmp(topic, "devices/1/pir1/")==0){
     pir1 = int(payload);}
-	if(strcmp(topic, "devices/1/stopalarm/")==0){
+    if(strcmp(topic, "devices/1/stopalarm/")==0){
      stopalarm = int(payload);}
   Serial.println();
 }
-
+ 
 PubSubClient client(mqtt_server, mqtt_port, callback, wclient);
-
+ 
 void subscribeToAllTopics(){
   for(int i=0;i<(sizeof(mqtt_topics)/sizeof(*mqtt_topics));i++)
     client.subscribe(mqtt_topics[i]);
 }
-
+ 
 void connectToWiFi(){
   if (WiFi.status() != WL_CONNECTED) {
     Serial.print("Connecting to ");
@@ -91,7 +89,7 @@ void connectToWiFi(){
     Serial.println("WiFi connected");
   }
 }
-
+ 
 void connectToMQTT(){
   String clientId = "ESP8266Client-";
   clientId += String(random(0xffff), HEX);
@@ -100,43 +98,27 @@ void connectToMQTT(){
     client.setServer(mqtt_server, mqtt_port);
     client.setCallback(callback);
     subscribeToAllTopics();
-   } 
+   }
   else
-    Serial.println("Could not connect to MQTT server"); 
+    Serial.println("Could not connect to MQTT server");
 }
-
+ 
 void playmelody()
 {
     static int ton = 100;  // тональность звука, Гц
     tone(soundPin, ton);  // включаем сирену на "ton" Гц
-    if (ton <= 500) {  // до частоты 500 Гц 
+    if (ton <= 500) {  // до частоты 500 Гц
         ton += 100;  // увеличиваем тональность сирены
     }
     else {  // по достижении 500 Гц
         ton = 100;  // сбрасываем тональность до 100 Гц
     }
 }
-
-void ledBlink() { 
+ 
+void ledBlink() {
     static bool ledStatus = false;    // состояние светодиода Вкл/Выкл
     ledStatus = !ledStatus;           // инвертируем состояние
     digitalWrite(redLed, ledStatus);  // включаем/выключаем светодиод
-}
-
-void loop()
-{
-  connectToWiFi();
-  if (WiFi.status() == WL_CONNECTED) {
-      if (!client.connected()) 
-      {
-        Serial.print("Connecting to MQTT server ");
-        Serial.print(mqtt_server);
-        Serial.println("...");
-        connectToMQTT();
-      }
-    if (client.connected()){
-  client.loop();}
-  if (pir1){working();}   
 }
 
 void working()
@@ -145,11 +127,11 @@ void working()
   if (digitalRead(PIR)==1)
   {
     if (led1)
-    {digitalWrite(bluLed, 0); 
+    {digitalWrite(bluLed, 0);
      ledThread.run();
     }
     if (sound){soundThread.run();}
-    client.publish("devices/1/situation", "Main Enter detected!")
+    client.publish("devices/1/situation", "Main Enter detected!");
     double currentTime = timer;
     while(timer-currentTime<10)
     {
@@ -163,4 +145,20 @@ void working()
     if (sms){client.publish("devices/1/timetokill/sms/", "Проникновение в Бэт-Пещеру из главного входа");}
     if (phone) {client.publish("devices/1/timetokill/phone/","Проникновение в Бэт-Пещеру из главного входа");}
   }
+ }
+void loop()
+{
+  connectToWiFi();
+  if (WiFi.status() == WL_CONNECTED) {
+      if (!client.connected())
+      {
+        Serial.print("Connecting to MQTT server ");
+        Serial.print(mqtt_server);
+        Serial.println("...");
+        connectToMQTT();
+      }
+    if (client.connected()){
+  client.loop();}
+  if (pir1){working();}  
+}
 }
