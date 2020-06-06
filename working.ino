c3254329250e03d38283093a62f064fd8edfd462
KindSpidey <44064808+KindSpidey@users.noreply.github.com>
2020-06-06 14:42:37 +0500

    ready

--- working.ino
+++ working.ino
@@ -5,6 +5,9 @@
  
 Thread soundThread = Thread();
 Thread ledThread = Thread();
+Thread pirThread = Thread();
+Thread checkSecThread = Thread();
+ 
  
 int PIR = 16;
 int redLed = 15;
@@ -12,14 +15,16 @@ int bluLed = 14;
 int grnLed = 12;
 int soundPin = 13;
 double timer;
-bool email = false;
-bool phone = false;
-bool led1 = false;
-bool pir1 = false;
-bool sound = false;
-bool sms = false;
-bool stopalarm = 1;
-byte sa = 1;
+bool email  ;
+bool phone  ;
+bool led1 ;
+bool pir1  ;
+bool sound ;
+bool sms ;
+bool stopalarm = false;
+bool blue;
+bool alarm;
+bool notpublish = true;
 const char *ssid = "WiFi-DOM.ru-0494"; // Имя роутера
 const char *pass = "xkfJDmMN28"; // Пароль роутера
  
@@ -43,33 +48,45 @@ void setup()
   pinMode(grnLed, OUTPUT);
   pinMode(bluLed, OUTPUT);
   Serial.begin(115200);
-  pir1 = 1;
-  led1 = 1;
-  sound = 1;
-  timer = millis();
   soundThread.onRun(playmelody);     // назначаем потоку задачу
-  soundThread.setInterval(20);
+  soundThread.setInterval(200);
   ledThread.onRun(ledBlink);  // назначаем потоку задачу
-  ledThread.setInterval(1000);
+  ledThread.setInterval(200);
+  pirThread.onRun(working);
+  pirThread.setInterval(500);
+  checkSecThread.onRun(checkSec);
+  checkSecThread.setInterval(500);
+}
+ 
+int convertFromPayload(byte* payload, unsigned int length){
+  char buf[length];
+  for(int i=0;i<length;i++)
+    buf[i] = (char) payload[i];
+  String value = String(buf);
+  return value.toInt();
 }
  
 void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("Message arrived [");
   Serial.print(topic);
   Serial.println("]");
+  int value = convertFromPayload(payload, length);
+  Serial.println(value);
    if(strcmp(topic, "devices/1/phone/")==0){
-   phone = convertFromPayload(payload, length);}
+   Serial.println(String(topic) + String(value));
+   phone = value;}
    if(strcmp(topic, "devices/1/email/")==0){
-   email = convertFromPayload(payload, length);}
+   email = value;}
+   if(strcmp(topic, "devices/1/sms/")==0){
+   sms = value;}
    if(strcmp(topic, "devices/1/led1/")==0){
-     led1 = convertFromPayload(payload, length);}
+   led1 = value; Serial.print("Led state: "); Serial.println(led1);}
    if(strcmp(topic, "devices/1/sound/")==0){
-   sound = convertFromPayload(payload, length);}
+   sound = value;}
    if(strcmp(topic, "devices/1/pir1/")==0){
-    pir1 = convertFromPayload(payload, length);}
+    pir1 = value;}
     if(strcmp(topic, "devices/1/stopalarm/")==0){
-     stopalarm = convertFromPayload(payload, length);}
-  Serial.println(*(int*)payload);
+    stopalarm = value;}
 }
  
 PubSubClient client(mqtt_server, mqtt_port, callback, wclient);
@@ -77,14 +94,7 @@ PubSubClient client(mqtt_server, mqtt_port, callback, wclient);
 void subscribeToAllTopics(){
   for(int i=0;i<(sizeof(mqtt_topics)/sizeof(*mqtt_topics));i++)
     client.subscribe(mqtt_topics[i]);
-}
-
-int convertFromPayload(byte* payload, unsigned int length){
-  char buf[length];
-  for(int i=0;i<length;i++)
-    buf[i] = (char) payload[i];
-  String value = String(buf);
-  return value.toInt();
+    client.subscribe("1234");
 }
  
 void connectToWiFi(){
@@ -113,51 +123,72 @@ void connectToMQTT(){
  
 void playmelody()
 {
-    static int ton = 100;  // тональность звука, Гц
-    tone(soundPin, ton);  // включаем сирену на "ton" Гц
-    if (ton <= 500) {  // до частоты 500 Гц
-        ton += 100;  // увеличиваем тональность сирены
-    }
-    else {  // по достижении 500 Гц
-        ton = 100;  // сбрасываем тональность до 100 Гц
-    }
-    noTone(soundPin);
+  if(sound && !stopalarm)
+    digitalWrite(soundPin, 1);
 //    Serial.println("SOUND WORKS!");
 }
  
-void ledBlink() {
-    static bool ledStatus = false;    // состояние светодиода Вкл/Выкл
-    ledStatus = !ledStatus;           // инвертируем состояние
-    digitalWrite(redLed, ledStatus);  // включаем/выключаем светодиод
-//    Serial.println("LED WORKS");
+void ledBlink()
+{
+  static unsigned long myLedTimer = millis();
+  if(millis()-myLedTimer>300){
+    myLedTimer = millis();
+    digitalWrite(bluLed, 0);
+    digitalWrite(redLed, 1-digitalRead(redLed));
+  }  
 }
- 
+void checkSec()
+{
+  Serial.print("Time:");
+  Serial.println(millis()-timer);
+  if (millis()-timer<3000 && stopalarm){
+    Serial.println("Turned off");
+    alarm=false;
+    stopalarm = false;
+    timer = millis();
+    digitalWrite(redLed, 0);
+    digitalWrite(soundPin, 0);
+  }
+  else if (millis()-timer>3000 && !stopalarm)
+  {
+      if (email){client.publish("devices/1/timetokill/email/", "Проникновение в Бэт-Пещеру из главного входа");
+                Serial.println("devices/1/timetokill/email/, Проникновение в Бэт-Пещеру из главного входа");}
+      if (sms){client.publish("devices/1/timetokill/sms/", "Проникновение в Бэт-Пещеру из главного входа");
+                Serial.println("devices/1/timetokill/sms/, Проникновение в Бэт-Пещеру из главного входа");}
+      if (phone) {client.publish("devices/1/timetokill/phone/","Проникновение в Бэт-Пещеру из главного входа");
+                Serial.println("devices/1/timetokill/phone/, Проникновение в Бэт-Пещеру из главного входа");}
+      Serial.println("Timed off");
+      alarm=false;
+      stopalarm = true;
+      timer = millis();
+      digitalWrite(redLed, 0);
+      digitalWrite(soundPin, 0);
+  }
+}
 void working()
 {
-  digitalWrite(bluLed, 1);
-  if (digitalRead(PIR)==1)
+  if (led1 && blue)
   {
-    if (led1)
-    {digitalWrite(bluLed, 0);
-     ledThread.run();
-    }
-    if (sound){soundThread.run();}
-    client.publish("devices/1/situation", "Main Enter detected!");
-    double currentTime = millis();
-    while(millis()-currentTime<10)
-    {
-      if (stopalarm)
+    digitalWrite(grnLed, 0);
+    digitalWrite(bluLed, 1);
+  }
+  if(!digitalRead(PIR)){
+    timer = millis();
+  }
+  if (!digitalRead(PIR) || alarm)
+  {  
+    alarm = true;
+      if (led1)
       {
-        stopalarm = false;
-        break;
-      }      
-    }
-    if (email){client.publish("devices/1/timetokill/email/", "Проникновение в Бэт-Пещеру из главного входа");
-              Serial.println("devices/1/timetokill/email/, Проникновение в Бэт-Пещеру из главного входа");}
-    if (sms){client.publish("devices/1/timetokill/sms/", "Проникновение в Бэт-Пещеру из главного входа");
-            Serial.println("devices/1/timetokill/sms/, Проникновение в Бэт-Пещеру из главного входа");}
-    if (phone) {client.publish("devices/1/timetokill/phone/","Проникновение в Бэт-Пещеру из главного входа");
-               Serial.println("devices/1/timetokill/phone/, Проникновение в Бэт-Пещеру из главного входа");}
+        digitalWrite(bluLed, 0);
+        ledThread.run();
+      }
+      if (sound && !stopalarm)
+        soundThread.run();
+      if (notpublish)
+        client.publish("devices/1/situation/", "Main Enter detected!");
+      checkSecThread.run();
+    notpublish = false;
   }
  }
 void loop()
@@ -172,7 +203,15 @@ void loop()
         connectToMQTT();
       }
     if (client.connected()){
-  client.loop();}
-  if (pir1){working();}  
+      client.loop();
+      if (led1 && !pir1){
+        digitalWrite(grnLed,1);
+        digitalWrite(bluLed,0);
+      }
+      if (pir1){
+        pirThread.run();
+        blue = true;
+      }
+  }
 }
 }
